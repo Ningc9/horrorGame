c4b0804f35f232c06d9543f719cf236881146cda
Eric Wang <kaedewang0726@gmail.com>
2025-03-27 12:54:57 +1100

    simple structure build

---
+++ Assets/Scripts/StartMenu/StartGameMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    // 退出游戏方法
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
