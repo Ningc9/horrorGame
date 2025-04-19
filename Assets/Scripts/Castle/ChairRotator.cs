d4a8413074c45277667f2f038513360a97f339dd
Eric Wang <kaedewang0726@gmail.com>
2025-04-19 14:55:13 +1000

    物体捡起扔出

---
+++ Assets/Scripts/Castle/ChairRotator.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairRotator : MonoBehaviour
+{
+    public float interactDistance = 3f; // 交互距离
+    private Transform player;
+    public float rotationAngle = 90f;
+
+    void Start()
+    {
+        GameObject playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.transform;
+        }
+        else
+        {
+            Debug.LogError("未找到玩家对象，请确保玩家有 'Player' 标签！");
+        }
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        if (distance <= interactDistance && Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (hit.collider.gameObject == gameObject && Input.GetKeyDown(KeyCode.E))
+            {
+                RotateChair();
+            }
+        }
+    }
+
+    void RotateChair()
+    {
+        transform.Rotate(0f, rotationAngle, 0f);
+        Debug.Log("椅子旋转了！");
+    }
+}
