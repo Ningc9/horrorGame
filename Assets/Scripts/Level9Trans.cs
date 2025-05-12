f3f7ab5c8eb4edf002b8c3762214d933fff58212
Eric Wang <kaedewang0726@gmail.com>
2025-05-12 11:55:22 +1000

    add the next level function

---
+++ Assets/Scripts/Level9Trans.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level9Trans : MonoBehaviour
+{
+    //public string nextSceneName; // 下一个场景的名称
+    public string playerTag = "Player"; // 玩家标签
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            Debug.Log("玩家离开城堡，加载下一个场景：");
+            //SceneManager.LoadScene(nextSceneName);
+            SceneManager.LoadScene("Level2");
+        }
+    }
+}
